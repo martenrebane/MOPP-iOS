06211252def2ec1c86664784f0edf459950b2d16
Marten Rebane <marten.rebane@nortal.com>
2021-04-23 15:49:17 +0300

    Coverity

--- .github/setup.sh
+++ .github/setup.sh
@@ -5,7 +5,8 @@ set -e
 # Environment check
 [ -z "$COVERITY_SCAN_PROJECT_NAME" ] && echo "ERROR: COVERITY_SCAN_PROJECT_NAME must be set" && exit 1
 [ -z "$COVERITY_SCAN_TOKEN" ] && echo "ERROR: COVERITY_SCAN_TOKEN must be set" && exit 1
-[ -z "$COVERITY_SCAN_KEY" ] && echo "ERROR: COVERITY_SCAN_TOKEN must be set" && exit 1
+[ -z "$COVERITY_SCAN_KEY" ] && echo "ERROR: COVERITY_SCAN_KEY must be set" && exit 1
+[ -z "$COVERITY_SCAN_GPG_KEY" ] && echo "ERROR: COVERITY_SCAN_GPG_KEY must be set" && exit 1
 
 # Create folder
 echo "Creating folder..."
@@ -17,7 +18,7 @@ COVERITY_TOOL_URL=https://scan.coverity.com/download/Darwin
 COVERITY_TOOL_KEY_URL=https://scan.coverity.com/download/key
 
 echo "Downloading key..."
-wget -nv -O $COVERITY_TOOL_DIR/$COVERITY_TOOL_KEY_NAME $COVERITY_TOOL_KEY_URL
+echo $COVERITY_SCAN_GPG_KEY >> $COVERITY_TOOL_DIR/$COVERITY_TOOL_KEY_NAME
 
 echo "Downloading tool..."
 wget -nv -O $COVERITY_TOOL_DIR/$COVERITY_TOOL_NAME $COVERITY_TOOL_URL --post-data "project=$COVERITY_SCAN_PROJECT_NAME&token=$COVERITY_SCAN_TOKEN"
